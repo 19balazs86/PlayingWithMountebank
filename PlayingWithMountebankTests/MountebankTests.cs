029cb7eb5ab6bc3e60e881a5f7fa39a6b5047dc7
Balázs Keresztúri <19balazs86@gmail.com>
2019-12-02 21:52:53 +0000

    Add test method for Json response

--- PlayingWithMountebankTests/MountebankTests.cs
+++ PlayingWithMountebankTests/MountebankTests.cs
@@ -11,8 +11,8 @@ namespace PlayingWithMountebankTests
 {
   public class MountebankTests : IDisposable
   {
-    private const int _port   = 4546;
-    private Uri _localhostUri = new Uri($"http://localhost:{_port}");
+    private const int _port = 4546;
+    private readonly Uri _localhostUri = new Uri($"http://localhost:{_port}");
 
     private readonly HttpClient _httpClient;
 
@@ -50,11 +50,46 @@ namespace PlayingWithMountebankTests
       Assert.Equal(statusCode, response.StatusCode);
     }
 
+    [Theory]
+    [InlineData(1)]
+    [InlineData(2)]
+    public async Task TestJsonResponse(int id)
+    {
+      // Arrange
+      string path = $"/user/{id}";
+      var user    = new User { Id = id, Name = $"Name_{id}" };
+
+      _imposter
+        .AddStub()
+        .OnPathAndMethodEqual(path, Method.Get)
+        .ReturnsJson(HttpStatusCode.OK, user);
+
+      _mountebankClient.Submit(_imposter);
+
+      // Act
+      using var response = await _httpClient.GetAsync(path);
+
+      // Assert
+      Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+      Assert.NotNull(response.Content);
+
+      User responseUser = await response.Content.ReadAsAsync<User>();
+
+      Assert.Equal(user.Id, responseUser.Id);
+      Assert.Equal(user.Name, responseUser.Name);
+    }
+
     public void Dispose()
     {
       _httpClient.Dispose();
 
       _mountebankClient.DeleteAllImposters();
     }
+  }
+
+  public class User
+  {
+    public int Id { get; set; }
+    public string Name { get; set; }
   }
 }
